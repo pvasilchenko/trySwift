a6f6d958dae9c00de1ba434c292c9b8e7855bcb3
Pavel <pavelvasilchenko96@gmail.com>
2017-11-11 19:23:51 +0200

    CheckList
    app added

---
+++ CheckLists/CheckLists/ChecklistViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CheckLists
+//
+//  Created by pVasilchenko on 08.11.2017.
+//  Copyright © 2017 pVasilchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
